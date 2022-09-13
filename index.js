e54dc09856808c670950621d6809a0428fb5e7ae
alaekwechekwube@gmail.com <alaekwechekwube@gmail.com>
2022-09-13 15:28:07 -0700

    updated code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 
-let Counter = (array) => {
-  var count = {};
-  array.forEach(val => count[val] = (count[val] || 0) + 1);
-  return count;
-}
-
 function nth_most_rate(list, n){
+  let count = {};
     dict = Counter(list)
+    function Counter(array){
+      array.forEach(val => count[val] = (count[val] || 0) + 1);
+      return count;
+    }
     for(let key in dict){
         if(n == dict[key])
-        return `The answer is ${key}, as ${n} is the rarest number`
+        return `The number is ${key}, as its the ${n} rarest number in the array`
     }
 }
 
-let lst = [5, 4, 5, 4, 5, 4, 4, 5, 3, 3, 3, 2, 2, 1, 5]
+let lst = [7, 5, 4, 5, 4, 5, 4, 4, 5, 3, 3, 3, 2, 2, 1, 5]
 
-console.log(nth_most_rate(lst, 2));
+console.log(nth_most_rate(lst, 4));
